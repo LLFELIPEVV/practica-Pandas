0966bf43a8a612be5e29c54e1c2fdc78b496f13a
FELIPE <felipe3133770868@gmail.com>
2025-02-01 17:41:30 -0500

    Agregar archivo de datos en formato JSON y ejemplos de lectura de JSON y diccionarios en Pandas.

--- pandas-1.py
+++ pandas-1.py
@@ -135,3 +135,58 @@ print(pd.options.display.max_rows)
 pd.options.display.max_rows = 9999
 print("Cambiar el maximo de filas:")
 print(df)
+
+# Leer archivos JSON
+# JSON tiene la misma estructura que un diccionario en Python.
+# Se puede convertir en un DataFrame.
+print()
+print("Leyendo archivos JSON")
+
+# Cargando un JSON en un DataFrame
+df = pd.read_json('data.json')
+
+print("Cargando un JSON en un DataFrame:")
+print(df.to_string())
+
+# Diccionario en formato JSON
+# Los objetos JSON se pueden convertir en diccionarios.
+# Un objeto JSON es un diccionario con claves y valores.
+data = {
+    "Duration": {
+        "0": "60",
+        "1": "60",
+        "2": "60",
+        "3": "45",
+        "4": "45",
+        "5": "60"
+    },
+    "Pulse": {
+        "0": "110",
+        "1": "117",
+        "2": "103",
+        "3": "109",
+        "4": "117",
+        "5": "102"
+    },
+    "Maxpulse": {
+        "0": "130",
+        "1": "145",
+        "2": "135",
+        "3": "175",
+        "4": "148",
+        "5": "127"
+    },
+    "Calories": {
+        "0": "409",
+        "1": "479",
+        "2": "340",
+        "3": "282",
+        "4": "406",
+        "5": "300"
+    }
+}
+
+df = pd.DataFrame(data)
+
+print(f"Diccionario en formato JSON:")
+print(df)
